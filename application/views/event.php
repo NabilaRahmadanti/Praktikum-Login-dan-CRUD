1a8857f9537f6999fd40d48e0217643b153a1b9d
Your Name <you@example.com>
2019-01-28 13:41:15 +0700

    selesai

---
+++ application/views/event.php
@@ -0,0 +1,8 @@
+<center><h1 class="judul">Event</h1>
+<div class="sale">
+    <center><img src="<?=base_url()?>assets/img/sale.png" alt="">
+</div>
+
+<div class="sale2">
+<center><img src="<?=base_url()?>assets/img/sale 2.png" alt="">
+</div>
---
+++ application/views/v_profil.php
@@ -0,0 +1,76 @@
+<h2 style="font-size: 35px; text-align:center;
+	font-family: novia;">Profil</h2>
+<!-- <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"> -->
+                    <div class="card">
+                        <div class="header">
+                            <div class="row clearfix">
+                                <div class="col-xs-12 col-sm-6">
+                                    <h2 style="  font-family: just tell me what;
+  font-size: 30px;
+  text-shadow: 2px 2px pink;
+  font-weight: bold;"></h2>
+                                </div>
+                                
+                        </div>
+                        <div class="body">
+                        	<div class="row">
+                            <div class="col-md-4">
+                            	<img src="<?=base_url('assets/foto.png')?>" style="width: 50%; margin-left: 53%;">
+                            </div>
+                            <div class="col-md-8">
+                            	<table class="table table-hover table-striped">
+                            		<tr>
+                            			<td>
+                            				Nama Lengkap
+                            			</td>
+                            			<td>
+                            				Nabila Rahmadanti Noviasari
+                            			</td>
+                            		</tr>
+                            		<tr>
+                                    <tr>
+                            			<td>
+                            				Nama Panggilan
+                            			</td>
+                            			<td>
+                            				Nabila / Bila
+                            			</td>
+                            		</tr>
+                            		<tr>
+                            			<td>
+                            				Tempat, tanggal lahir
+                            			</td>
+                            			<td>
+                            				Malang, 22 November 2001
+                            			</td>
+                            		</tr>
+                            		<tr>
+                            			<td>
+                            				Umur
+                            			</td>
+                            			<td>
+                            				17 tahun
+                            			</td>
+                            		</tr>
+                                    <tr>
+                            			<td>
+                            				Sekolah
+                            			</td>
+                            			<td>
+                            				SMK Telkom Malang
+                            			</td>
+                            		</tr>
+                            		<tr>
+                            			<td>
+                            				Alamat
+                            			</td>
+                            			<td>
+                            				Sawojajar, Malang
+                            			</td>
+                            		</tr>
+                            	</table>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
